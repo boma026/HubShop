fc885199544e13d3e2213c887480aa87e7f26847
boma <arthurboma@yahoo.com.br>
2025-10-27 06:23:19 -0300

    iniciado o projeto do backend junto a estruturação do model, do banco e das entidades

---
+++ back-end/core/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Sale(models.Model):
+    customer = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    products = models.ManyToManyField('Product', through='SaleProduct')
+
+    def total(self):
+        return sum(item.product.price * item.quantity for item in self.saleproduct_set.all())
+
+    def __str__(self):
+        return f"Sale #{self.id} - {self.customer}"
+
+
+class SaleProduct(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+
+class Purchase(models.Model):
+    supplier = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE, related_name='purchases')
+    products = models.ManyToManyField('Product', through='PurchaseProduct')
+
+    def total(self):
+        return sum(item.product.price * item.quantity for item in self.purchaseproduct_set.all())
+
+    def __str__(self):
+        return f"Purchase #{self.id} - {self.supplier}"
+
+
+class PurchaseProduct(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
